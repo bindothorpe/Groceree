9ac9332b60abfef85b36ccfc66416d54dba368cd
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-18 13:33:44 +0100

    Initial Commit

---
+++ Groceree/GrocereeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrocereeApp.swift
+//  Groceree
+//
+//  Created by Bindo Thorpe on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GrocereeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrocereeTests/GrocereeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrocereeTests.swift
+//  GrocereeTests
+//
+//  Created by Bindo Thorpe on 18/11/2024.
+//
+
+import Testing
+@testable import Groceree
+
+struct GrocereeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
